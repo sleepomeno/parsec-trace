699bb196471aa5dfd1e12932bfb61228eb660905
Gregor Riegler <gregor.riegler@gmail.com>
2015-10-30 14:19:33 +0100

    Add basic haddock documentation

--- src/Text/Parsec/Trace/Tree.hs
+++ src/Text/Parsec/Trace/Tree.hs
@@ -1,95 +1,160 @@
--- {-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction  #-}
+{-|
+Module      : Text.Parsec.Trace.Tree
+Description : Add a tree tracing the successful parsers in the Parsec user state 
+Copyright   : (c) Gregor Riegler, 2015
+License     : MIT
+Maintainer  : gregor.riegler@gmail.com
+Stability   : experimental
+-}
+
+{-# LANGUAGE FlexibleContexts, FunctionalDependencies, MultiParamTypeClasses, ScopedTypeVariables  #-}
 
 module Text.Parsec.Trace.Tree (
     TraceTree
-  , TraceConfig
+  -- * Classes
   , HasTraceTree
-  , defaultTraceConfig
   , getTrace
   , modTrace
+
+  -- * Trace configuration
+  , TraceConfig
+  , defaultTraceConfig
+
+  -- ** Setting fields of the trace configuration
   , setTraceFunction
   , setLogEnter
   , setLogExit
+
+  -- ** Lenses for modifying a trace configuration
   , _traceParser
   , _logEnter
   , _logExit
 
-  , log
-  , drawTraceTree
-  , getTraceTree
+  -- * Provide tracing to parsers
   , traceWith
   , initialTraceTree
+  , logP
+
+  -- * Processing the user state providing the trace tree (after parsing)
+  , drawTraceTree
+  , drawTraceTree'
+  , getTraceTree
     ) where
 
-import Prelude hiding (last,log)
+import Prelude hiding (last)
 
-import Text.Parsec hiding (parse)
+import Text.Parsec
 import Data.Tree
+import Data.String
 import Data.Tree.Zipper
-import Control.Monad.State
+import Control.Monad.Trans
 import Data.Maybe
 
-type TraceTree = TreePos Full String
+type TraceTree a = TreePos Full a
 
-class HasTraceTree t where
-  getTrace :: t -> TraceTree
-  modTrace :: (TraceTree -> TraceTree) -> t -> t
+-- | An instance of 'HasTraceTree' somehow refers to a 'TraceTree'
+--
+--  Make your Parsec user state type an instance of this class.
+class (IsString a) => HasTraceTree t a | t -> a where
+  -- | Get the 'TraceTree' from the parser state 't'
+  getTrace :: t -> TraceTree a
+  -- | Modify the 'TraceTree' stored in the parser state 't'
+  modTrace :: (TraceTree a -> TraceTree a) -> t -> t
 
-data TraceConfig a u m = TraceConfig { traceParser :: a -> String
-                                     , logEnter    :: Maybe (u -> m String)
-                                     , logExit     :: Maybe (u -> m String) }
+-- | 'e' is the value type of your parsers
+--
+--   's' is the 'IsString' instance type of the tree values
+--
+--   'u' is the Parsec user state
+--
+--   'm' is the underlying monad of your Parsec parsers
+data TraceConfig e s u m = TraceConfig { traceParser :: e -> s
+                                       , logEnter    :: Maybe (u -> m s)
+                                       , logExit     :: Maybe (u -> m s) }
 
--- setTraceFunction :: (a -> String) -> TraceConfig t u m -> TraceConfig a u m
+setTraceFunction :: (IsString s ) =>
+                    (e -> s)                  -- ^ How to produce a tree value given the result of a parser
+                 -> TraceConfig e s u m       
+                 -> TraceConfig e s u m
 setTraceFunction f conf = conf { traceParser = f }
 
--- setLogEnter :: (u -> m String) -> TraceConfig a u m -> TraceConfig a u m
+setLogEnter :: (IsString s ) =>
+               (u -> m s)  -- ^ Logging action that is run at the start of a parser
+            -> TraceConfig a s u m -> TraceConfig a s u m
 setLogEnter lEn conf = conf { logEnter = Just lEn }
 
--- setLogExit :: (u -> m String) -> TraceConfig a u m -> TraceConfig a u m
+setLogExit :: (IsString s) =>
+              (u -> m s)   -- ^ Logging action that is run at the end of the parser
+           -> TraceConfig a s u m -> TraceConfig a s u m
 setLogExit lEx conf = conf { logExit = Just lEx }
 
-_traceParser :: Functor f => ((a -> String) -> f (a -> String)) -> TraceConfig a u m -> f (TraceConfig a u m)
+_traceParser :: (Functor f, IsString s) =>
+                ((a -> s) -> f (a -> s)) -> TraceConfig a s u m -> f (TraceConfig a s u m)
 _traceParser f (TraceConfig t lEn lEx) = fmap (\t' -> TraceConfig t' lEn lEx) (f t)
 
-_logEnter :: Functor f => (Maybe (u -> m String) -> f (Maybe (u -> m String))) -> TraceConfig a u m -> f (TraceConfig a u m)
+_logEnter :: (Functor f, IsString s) =>
+             (Maybe (u -> m s) -> f (Maybe (u -> m s))) -> TraceConfig a s u m -> f (TraceConfig a s u m)
 _logEnter f (TraceConfig t lEn lEx) = fmap (\lEn' -> TraceConfig t lEn' lEx) (f lEn)
 
-_logExit :: Functor f => (Maybe (u -> m String) -> f (Maybe (u -> m String))) -> TraceConfig a u m -> f (TraceConfig a u m)
+_logExit :: (Functor f, IsString s) =>
+            (Maybe (u -> m s) -> f (Maybe (u -> m s))) -> TraceConfig a s u m -> f (TraceConfig a s u m)
 _logExit f (TraceConfig t lEn lEx) = fmap (TraceConfig t lEn) (f lEx)
 
-initialTraceTree = fromTree (Node "Program" [])
+-- | The value that can be used on initialisation of the Parsec user state
+initialTraceTree :: (IsString s) => TraceTree s
+initialTraceTree = fromTree (Node (fromString "Program") [])
 
-                                     
-defaultTraceConfig :: Show a => TraceConfig a u m
-defaultTraceConfig = TraceConfig show Nothing Nothing
+-- | Default configuration which logs nothing on entering/exiting and ignores the parser values
+-- Manipulate this default configuration with setters as 'setLogEnter' or lenses as '_logEnter'
+defaultTraceConfig :: (IsString s) => TraceConfig e s u m
+defaultTraceConfig = TraceConfig (const (fromString "")) Nothing Nothing
 
+-- | Apply this function to your 'TraceConfig' to get a function which adds tracing to a parser
+-- 
+-- @
+--    trace = traceWith (setTraceFunction show defaultTraceConfig)
+-- 
+--    myparser = trace $ do { string "parseSomething" }
+-- @
+traceWith :: (Monad m, IsString s, HasTraceTree u s) => TraceConfig e s u m -> ParsecT t u m e -> ParsecT t u m e
+traceWith (TraceConfig traceParser logEnter logExit) p = tracedWithLog traceParser p logEnter logExit
+   where
+    tracedWithLog :: (Monad m, IsString s, HasTraceTree u s) =>
+                     (expr -> s) ->
+                     ParsecT t u m expr ->
+                     Maybe (u -> m s) ->
+                     Maybe (u -> m s) ->
+                     ParsecT t u m expr
+    tracedWithLog f p logInit logExit = do
+        modifyState . modTrace $ insert (Node (fromString "") []) . last . children
 
-log :: (Monad m, HasTraceTree u) => (u -> m String) -> String -> ParsecT s u m () 
-log action caption = do
+        _ <- case logInit of { Nothing -> return (); Just init -> logP init }
+
+        result <- p
+
+        modifyState . modTrace $ modifyTree (\t -> t { rootLabel = f result })
+
+        case logExit of { Nothing -> return (); Just exit -> logP exit }
+
+        modifyState . modTrace $ fromJust . parent
+
+        return result
+
+drawTraceTree :: HasTraceTree t a => (a -> String) -> t -> String
+drawTraceTree f = drawTree . fmap f . tree . getTrace 
+
+drawTraceTree' :: HasTraceTree t String => t -> String
+drawTraceTree' = drawTree . tree . getTrace 
+
+getTraceTree :: (HasTraceTree t s, IsString s) => t -> Tree s
+getTraceTree = tree . getTrace 
+
+-- | Use 'logP' to log a monadic value in the 'TraceTree' as a leaf of the current parser
+logP :: (Monad m, HasTraceTree u s, IsString s) => (u -> m s) -> ParsecT t u m () 
+logP action = do
   s <- getState
-  result <- lift $ (caption ++) <$> action s
+  result <- lift $ action s
   modifyState . modTrace $ fromJust . parent . insert (Node result []) . last . children
 
-tracedWithLog f p logInit logExit = do
-  modifyState . modTrace $ insert (Node "" []) . last . children
-
-  case logInit of { Nothing -> return (); Just init -> log init "On Enter: " }
-
-  result <- p
-
-  modifyState . modTrace $ modifyTree (\t -> t { rootLabel = f result })
-
-  case logExit of { Nothing -> return (); Just exit -> log exit "On Enter: " }
-
-  modifyState . modTrace $ fromJust . parent
-
-  return result
   
 
-traceWith (TraceConfig traceParser logEnter logExit) p = tracedWithLog traceParser p logEnter logExit
-
-drawTraceTree :: HasTraceTree s => s -> String
-drawTraceTree = drawTree . tree . getTrace 
-
-getTraceTree :: HasTraceTree s => s -> Tree String
-getTraceTree = tree . getTrace 
